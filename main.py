2ec13d7de5a3727a999c23a443721b39ea79f45b
felipedebarros <feliperbar@gmail.com>
2017-10-09 21:37:14 -0300

    Fixed the correct counter
    it works now

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from mnist import MNIST
 from PyWANN import WiSARD
 import time
+import numpy as np
+from random import randint
 
 print("starting...")
 start_time = time.time()
 print("loading images...")
 
-threshold = 64
+activation = 127
 
 def transform(retina):
     for i in range(0, len(retina)):
         for j in range(0, len(retina[i])):
-            if retina[i][j] > threshold:
+            if retina[i][j] > activation:
                 retina[i][j] = 1
             else:
                 retina[i][j] = 0
@@ -26,11 +28,10 @@ testing_images = transform(testing_images)
 
 print("images loaded!")
 
-retina_length = 28*28
 num_bits_addr = 28
-bleaching = False
+bleaching = True
 
-w = WiSARD.WiSARD(num_bits_addr, bleaching)
+w = WiSARD.WiSARD(num_bits_addr, bleaching, seed = int(time.time()))
 
 print("beginning training...")
 
@@ -41,15 +42,16 @@ print("training complete!")
 print("beginning tests...")
 
 # predicting class
-result = w.predict(testing_images)  #  Result will be a dictionary using the classes as key and the WiSARD result as values
+result = np.array(w.predict(testing_images))  #  Result will be a dictionary using the classes as key and the WiSARD result as values
+correct_items = np.sum(np.array(testing_labels) == result) # Return only the items present in both lists
 
-correct_items = set(testing_labels) & set(result) # Return only the items present in both lists
 
 print("tests complete")
 print("waiting results...")
 
-acc = float(len(correct_items)) / float(len(testing_labels)) * 100
-print(str(len(correct_items)) + ' / ' + str(len(testing_labels)) + ' = ' + str( acc ) + '% de acerto')
+acc = float(correct_items) / float(len(testing_labels)) * 100
+print(str(correct_items) + ' / ' + str(len(testing_labels)) + ' = ' + str( acc ) + '% de acerto')
+print("Retina cell activation threshold: " + str(activation))
 print("number of bits in address: " + str(num_bits_addr))
 print("using bleaching: " + str(bleaching))
 print("elapsed time: %s seconds" % (time.time() - start_time))
